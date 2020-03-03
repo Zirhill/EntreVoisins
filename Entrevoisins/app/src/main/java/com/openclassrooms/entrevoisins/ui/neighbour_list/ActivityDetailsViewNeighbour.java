b590ff85e1c0d2fd8d83dadc5dc87d4a563b1772
Zirhil <buffardcyril@yahoo.fr>
2020-03-03 22:02:18 +0100

    apres le cours de mardi

--- Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ActivityDetailsViewNeighbour.java
+++ Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ActivityDetailsViewNeighbour.java
@@ -1,15 +1,35 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.openclassrooms.entrevoisins.R;
 
+import butterknife.BindView;
+
 public class activity_details_view_neighbour extends AppCompatActivity {
+
+    private String nom;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_details_view_neighbour);
+
+        Intent intent = new Intent(this, activity_details_view_neighbour.class);
+                TextView nom = findViewById(R.id.textView_name);
+
+                String nameTv = nom.getText().toString();
+                intent.putExtra(Intent.EXTRA_COMPONENT_NAME, nameTv);
+
+
     }
 }
